76bc13d4115668c54b2693f42358a1190f8d38a4
Le Vivilet <levivlet@pm.me>
2025-04-25 22:18:54 +0200

    feature: sort commands

--- packages/file-system-process/src/parts/CommandMap/CommandMap.ts
+++ packages/file-system-process/src/parts/CommandMap/CommandMap.ts
@@ -3,6 +3,7 @@ import * as FileSystemDisk from '../FileSystemDisk/FileSystemDisk.ts'
 export const commandMap = {
   'FileSystem.chmod': FileSystemDisk.chmod,
   'FileSystem.copy': FileSystemDisk.copy,
+  'FileSystem.getFolderSize': FileSystemDisk.getFolderSize,
   'FileSystem.getPathSeparator': FileSystemDisk.getPathSeparator,
   'FileSystem.mkdir': FileSystemDisk.mkdir,
   'FileSystem.readDirWithFileTypes': FileSystemDisk.readDirWithFileTypes,
@@ -12,5 +13,4 @@ export const commandMap = {
   'FileSystem.rename': FileSystemDisk.rename,
   'FileSystem.stat': FileSystemDisk.stat,
   'FileSystem.writeFile': FileSystemDisk.writeFile,
-  'FileSystem.getFolderSize': FileSystemDisk.getFolderSize,
 }
