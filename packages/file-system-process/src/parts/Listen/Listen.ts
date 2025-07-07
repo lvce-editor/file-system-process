2696b51a86dde514d84d2cfa18ad7ea36ac18f0a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-07 21:07:26 +0200

    feature: add shared process rpc (#63)

--- packages/file-system-process/src/parts/Listen/Listen.ts
+++ packages/file-system-process/src/parts/Listen/Listen.ts
@@ -1,10 +1,12 @@
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
+import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
 
 export const listen = async (argv: readonly string[]): Promise<void> => {
-  await IpcChild.listen({
+  const rpc = await IpcChild.listen({
     method: IpcChildType.Auto(argv),
     commandMap: CommandMap.commandMap,
   })
+  SharedProcess.set(rpc)
 }
