b7465b48ac0a4a6ccd317f469ac7de35b3e02ddc
Le Vivilet <levivlet@pm.me>
2025-07-08 00:30:04 +0200

    tsc

--- packages/file-system-process/test/Initialize.test.ts
+++ packages/file-system-process/test/Initialize.test.ts
@@ -4,7 +4,3 @@ import * as Initialize from '../src/parts/Initialize/Initialize.js'
 test('initialize is a function', () => {
   expect(typeof Initialize.initialize).toBe('function')
 })
-
-test.skip('initialize throws error for invalid port', async () => {
-  await expect(Initialize.initialize(null as any)).rejects.toThrow()
-})
