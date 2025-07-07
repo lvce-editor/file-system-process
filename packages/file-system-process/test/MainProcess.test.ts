dcd27953a48f9edb23aa9ee7ba7d39e0ecf47ab8
Le Vivilet <levivlet@pm.me>
2025-07-08 00:23:08 +0200

    test

--- packages/file-system-process/test/MainProcess.test.ts
+++ packages/file-system-process/test/MainProcess.test.ts
@@ -5,10 +5,6 @@ test('MainProcess exports invoke function', () => {
   expect(typeof MainProcess.invoke).toBe('function')
 })
 
-test('MainProcess exports invokeAndTransfer function', () => {
-  expect(typeof MainProcess.invokeAndTransfer).toBe('function')
-})
-
 test('MainProcess exports set function', () => {
   expect(typeof MainProcess.set).toBe('function')
 })
