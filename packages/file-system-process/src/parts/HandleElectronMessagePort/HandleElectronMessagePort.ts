ed787b4fc4cf2e8e6b86b18940e26395732d7fc2
Le Vivilet <levivlet@pm.me>
2025-07-07 23:30:44 +0200

    feature: improve shared process rpc

--- packages/file-system-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
+++ packages/file-system-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
@@ -1,11 +1,15 @@
+import { set } from '@lvce-editor/rpc-registry'
 import * as Assert from '../Assert/Assert.ts'
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
-export const handleElectronMessagePort = async (messagePort: any, ...params: readonly any[]): Promise<void> => {
+export const handleElectronMessagePort = async (messagePort: any, rpcId?: number): Promise<void> => {
   Assert.object(messagePort)
-  await IpcChild.listen({
+  const rpc = await IpcChild.listen({
     method: IpcChildType.ElectronMessagePort,
     messagePort,
   })
+  if (rpcId) {
+    set(rpcId, rpc)
+  }
 }
