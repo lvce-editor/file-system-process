da58d8439ce7a484e73800b74410eeaecaf8a130
Le Vivilet <levivlet@pm.me>
2025-08-15 10:54:04 +0200

    feature: add basic file watcher

--- packages/file-system-process/src/parts/FileWatcher/FileWatcher.ts
+++ packages/file-system-process/src/parts/FileWatcher/FileWatcher.ts
@@ -1,7 +1,43 @@
-export const watchFile = (uri: string): void => {
-  // TODO
+import * as fs from 'node:fs/promises'
+import { assertUri } from '../AssertUri/AssertUri.ts'
+import { fileURLToPath } from 'node:url'
+
+const fileWacherEvents: Record<number, readonly any[]> = Object.create(null)
+
+const setupWatcher = async (watcherId: number, uri: string, onChange: () => void): Promise<void> => {
+  try {
+    const path = fileURLToPath(uri)
+    // TODO
+    const watcher = fs.watch(path, {})
+    const watcherEvents: any[] = []
+    fileWacherEvents[watcherId] = watcherEvents
+    for await (const event of watcher) {
+      watcherEvents.push(event)
+      onChange()
+    }
+  } catch (error) {
+    if (error && error.code === 'ENOENT') {
+      return
+    }
+    console.error(error)
+  }
+}
+
+export const watchFile = async (watcherId: number, uri: string): Promise<void> => {
+  assertUri(uri)
+  fileWacherEvents[watcherId] = []
+  // TODO await promise?
+  const onChange = () => {
+    // console.log('did change')
+    // TODO send response via socket / messageport
+  }
+  setupWatcher(watcherId, uri, onChange)
 }
 
 export const unwatchFile = (uri: string): void => {
   // TODO
+}
+
+export const getEvents = (watcherId: number): readonly any[] => {
+  return fileWacherEvents[watcherId] || []
 }
