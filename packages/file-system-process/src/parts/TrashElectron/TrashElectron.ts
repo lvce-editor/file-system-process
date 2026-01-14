8c52b9e3f2a158dc175d97ca8ff756008716ec06
Le Vivilet <levivilet@pm.me>
2026-01-14 19:34:12 +0100

    feature: use lazy main process rpc

--- packages/file-system-process/src/parts/TrashElectron/TrashElectron.ts
+++ packages/file-system-process/src/parts/TrashElectron/TrashElectron.ts
@@ -1,10 +1,9 @@
+import { MainProcess } from '@lvce-editor/rpc-registry'
 import { existsSync } from 'node:fs'
-import * as MainProcess from '../MainProcess/MainProcess.ts'
 
 export const trash = async (path: string): Promise<void> => {
   if (!existsSync(path)) {
     return
   }
-  // @ts-ignore
   await MainProcess.invoke('Trash.trash', path)
 }
